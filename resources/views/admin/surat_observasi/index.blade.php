072475c2aa156504c00edf927640a15ee8df7e2b
ricomrdnsyh <ricomardiansyah27@gmail.com>
2025-11-16 15:00:18 +0700

    change action surat role admin

--- resources/views/admin/surat_observasi/index.blade.php
+++ resources/views/admin/surat_observasi/index.blade.php
@@ -97,12 +97,12 @@
                             <thead class="">
                                 <!--begin::Table row-->
                                 <tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
+                                    <th class="text-center">Actions</th>
                                     <th class="min-w-125px">Nama Mahasiswa</th>
                                     <th class="min-w-125px">Program Studi</th>
                                     <th class="min-w-125px">Tanggal Pengajuan</th>
                                     <th class="min-w-125px">Status Pengajuan</th>
                                     <th class="min-w-125px">Catatan</th>
-                                    <th class="text-center">Actions</th>
                                 </tr>
                                 <!--end::Table row-->
                             </thead>
@@ -142,6 +142,11 @@
                     }
                 },
                 columns: [{
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    }, {
                         data: 'nama_mahasiswa',
                         name: 'nama_mahasiswa',
                         searchable: true
@@ -160,12 +165,6 @@
                     {
                         data: 'catatan',
                         name: 'catatan'
-                    },
-                    {
-                        data: 'action',
-                        name: 'action',
-                        orderable: false,
-                        searchable: false
                     }
                 ],
 
